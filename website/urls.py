431ab1879abb7fe7f8e54cafe4b991997e159f2b
Krona Emmanuel <kronaemmanuel@gmail.com>
2022-07-24 00:33:34 +0500

    Add Login, Logout, and Index page

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 app_name = 'website'
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
     path('register', views.register, name='register')
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,37 @@
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib import auth
+
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('website:index')
+        else:
+            messages.error(request, 'Error wrong username/password')
+
+    return render(request, 'website/login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return render(request, 'website/logout.html')
+
+
+def index(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    return render(request, 'website/index.html')
 
 
 def register(request):
