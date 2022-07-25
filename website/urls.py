9c4388c8ba9c77eca786abed3698e75ca3120ca9
Krona Emmanuel <kronaemmanuel@gmail.com>
2022-07-25 11:36:13 +0500

    Add User Details Update View

--- website/urls.py
+++ website/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
-    path('register', views.register, name='register')
+    path('register', views.register, name='register'),
+    path('user_update', views.user_update, name='user_update')
 ]
